65e7d15121ccf7b7cf2df1559b118cd92e44d522
RojaPravin <mroja1412@gmail.com>
2024-12-09 16:41:58 +0530

    Add project files.

---
+++ Controllers/billingController.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.SqlClient;
+using Snacks_withoutEntity.Models;
+
+namespace Snacks_withoutEntity.Controllers
+{
+    public class billingController : Controller
+    {
+        string dbconnection = @"Data Source = DESKTOP-RNPM1SO; Initial Catalog = Snacks; Integrated Security = True ";
+        // GET: billing
+        public ActionResult Index()
+        {
+            List<billing> billingobj = new List<billing>();
+            using (SqlConnection con = new SqlConnection(dbconnection)) 
+            {
+                SqlCommand cmd = new SqlCommand("sp_billing_fetch", con);
+                con.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                while(reader.Read())
+                {
+                    billingobj.Add(new billing
+                    {
+                        Id = Convert.ToInt32(reader["id"].ToString()),
+                        Name = reader["Name"].ToString(),
+                        Cost = Convert.ToInt32(reader["Cost"].ToString()),
+                        Company = reader["Company"].ToString(),
+                    });
+
+                }
+                con.Close();
+            }
+            return View (billingobj);
+        }
+
+        // GET: billing/Details/5
+        public ActionResult Details(int id)
+        {
+            billing billingobj = new billing();
+            using (SqlConnection con = new SqlConnection(dbconnection))
+            {
+                SqlCommand cmd = new SqlCommand("sp_billing_fetch_id " + id , con);
+                con.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    billingobj = new billing
+                    {
+                        Id = Convert.ToInt32(reader["id"].ToString()),
+                        Name = reader["Name"].ToString(),
+                        Cost = Convert.ToInt32(reader["Cost"].ToString()),
+                        Company = reader["Company"].ToString(),
+                    };
+
+                }
+                con.Close();
+            }
+            return View(billingobj);
+        }
+
+        // GET: billing/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: billing/Create
+        [HttpPost]
+        public ActionResult Create(billing billingobj)
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(dbconnection))
+                {
+                    string query = "sp_billing_insert '" + billingobj.Name + "'," + billingobj.Cost + ",'" + billingobj.Company + "'";
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return RedirectToAction("Index");
+            }
+            
+        }
+
+        // GET: billing/Edit/5
+        public ActionResult Edit(int id)
+        {
+            billing billingobj = new billing();
+            using (SqlConnection con = new SqlConnection(dbconnection))
+            {
+                SqlCommand cmd = new SqlCommand("sp_billing_fetch_id " + id, con);
+                con.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    billingobj = new billing
+                    {
+                        Id = Convert.ToInt32(reader["id"].ToString()),
+                        Name = reader["Name"].ToString(),
+                        Cost = Convert.ToInt32(reader["Cost"].ToString()),
+                        Company = reader["Company"].ToString(),
+                    };
+
+                }
+                con.Close();
+            }
+            return View(billingobj);
+        }
+
+        // POST: billing/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, billing billingobj)
+        {
+
+            try
+            {
+                using (SqlConnection con = new SqlConnection(dbconnection))
+                {
+                    string query = "sp_billing_edit " + id + ",'" + billingobj.Name + "'," + billingobj.Cost + ",'" + billingobj.Company + "'";
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+
+                    con.Close();
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return RedirectToAction("Index");
+            }
+        }
+
+        // GET: billing/Delete/5
+        public ActionResult Delete(int id)
+        {
+            billing billingobj = new billing();
+            using (SqlConnection con = new SqlConnection(dbconnection))
+            {
+                SqlCommand cmd = new SqlCommand("sp_billing_fetch_id " + id, con);
+                con.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    billingobj = new billing
+                    {
+                        Id = Convert.ToInt32(reader["id"].ToString()),
+                        Name = reader["Name"].ToString(),
+                        Cost = Convert.ToInt32(reader["Cost"].ToString()),
+                        Company = reader["Company"].ToString(),
+                    };
+
+                }
+                con.Close();
+            }
+            return View(billingobj);
+        }
+
+        // POST: billing/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, billing billingobj)
+        {
+
+            try
+            {
+                using (SqlConnection con = new SqlConnection(dbconnection))
+                {
+                    string query = "sp_billing_delete " + id ;
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
---
+++ Models/billing.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Snacks_withoutEntity.Models
+{   
+
+    public class billing
+    {  
+          //[table("billing")]
+            public int Id { get; set; }
+            [StringLength (100)]
+            [Required (ErrorMessage = " Snacks Name is required")]
+            public string Name { get; set; }
+            public int? Cost { get; set; }
+            public string Company { get; set; }
+
+        
+    }
+
+}
